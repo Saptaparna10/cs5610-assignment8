186a4f596ff17b352986705483ecd8447e69b0b2
Saptaparna10 <saptaparna1991@gmail.com>
2019-04-09 20:50:11 -0400

    heroku fix

--- server.js
+++ server.js
@@ -1,34 +1,17 @@
-// require express
-var express = require('express')
-var app = express();
-const path = require('path');
+const express = require("express")
+var bodyParser = require('body-parser');
+var session = require('express-session')
 
+const app = express()
 
-// require body-parser
-var bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'any string'
+}));
 
-
-// app.use(session({
-//     resave: false,
-//     saveUninitialized: true,
-//     secret: 'any string'
-// }));
-//
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin",
-        req.headers.origin);
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./data/db')()
 
@@ -38,13 +21,4 @@ require('./data/db')()
 require('./data/services/university.service.server')(app)
 
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-
-    res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-});
+app.listen(3000)
