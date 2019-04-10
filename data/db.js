f38ae77feb2f9c268c5569f69d6811888b695029
Saptaparna10 <saptaparna1991@gmail.com>
2019-04-10 09:03:34 -0400

    mlab config

--- data/db.js
+++ data/db.js
@@ -1,7 +1,8 @@
 module.exports = function () {
     const mongoose = require('mongoose');
-    mongoose.connect("mongodb://sappy:spssbbpp10@ds219181.mlab.com:19181/sappy", { useNewUrlParser: true });
+    mongoose.connect("mongodb://heroku_d5p8fkm7:psa94a3sk2eajvp314s1b6qq1p@ds133632.mlab.com:33632/heroku_d5p8fkm7", { useNewUrlParser: true });
 
+    //mongodb://heroku_d5p8fkm7:psa94a3sk2eajvp314s1b6qq1p@ds133632.mlab.com:33632/heroku_d5p8fkm7
     //mongodb://localhost:27017/testdb
     //mongodb://sappy:spssbbpp10@ds219181.mlab.com:19181/sappy
 }
